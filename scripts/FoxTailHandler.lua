600d247df5a31797ce5df2282b59597206b2bee6
AgentVen <agentven0x@gmail.com>
2024-12-29 18:03:46 -0700

    (retry at commit) AFTER WEEKS! I (think I) FINALLY FIGURED OUT WHY THIS WASN'T WORKING! (see the 'comment chain' starting at line#16 in the FoxTailHandler.lua script)

--- scripts/FoxTailHandler.lua
+++ scripts/FoxTailHandler.lua
@@ -39,84 +39,35 @@ local VERTICES = {
 	["EDS"] = 7;	["WDS"] = 9;	["USW"] = 1;	["DSW"] = 16;
 }
 
-local VERTEX_PAIRINGS = {
-	-- Vertex #1+ are set to the position of Vertex #0
-	{ [0] = "1gNUE", "0fUSE", "0fESU" },
-	{ [0] = "1gNUW", "0fUSW", "0fWSU" },
-	{ [0] = "1gNDE", "0fDSE", "0fESD" },
-	{ [0] = "1gNDW", "0fDSW", "0fWSD" };
-
-	{ [0] = "2gNUE", "1fUSE", "1fESU" },
-	{ [0] = "2gNUW", "1fUSW", "1fWSU" },
-	{ [0] = "2gNDE", "1fDSE", "1fESD" },
-	{ [0] = "2gNDW", "1fDSW", "1fWSD" };
-
-	{ [0] = "3gNUE", "2fUSE", "2fESU" },
-	{ [0] = "3gNUW", "2fUSW", "2fWSU" },
-	{ [0] = "3gNDE", "2fDSE", "2fESD" },
-	{ [0] = "3gNDW", "2fDSW", "2fWSD" };
-
-	{ [0] = "3gSUE", "4fUNE", "4fENU" },
-	{ [0] = "3gSUW", "4fUNW", "4fWNU" },
-	{ [0] = "3gSDE", "4fDNE", "4fEND" },
-	{ [0] = "3gSDW", "4fDNW", "4fWND" };
-
-	{ [0] = "4gSUE", "5fUNE", "5fENU" },
-	{ [0] = "4gSUW", "5fUNW", "5fWNU" },
-	{ [0] = "4gSDE", "5fDNE", "5fEND" },
-	{ [0] = "4gSDW", "5fDNW", "5fWND" };
-
-	{ [0] = "5gSUE", "6fUNE", "6fENU" },
-	{ [0] = "5gSUW", "6fUNW", "6fWNU" },
-	{ [0] = "5gSDE", "6fDNE", "6fEND" },
-	{ [0] = "5gSDW", "6fDNW", "6fWND" };
-}
-
-
-
-
-local function addVerticeOriginVectors()
-	-- Add origin pos, uv, and normal vectors to vertices defined in VERTICES table.
-	for k,v in pairs(VERTICES) do
-		v.originPos = v.Part:getVertices(TEXTURE)[v.VertexNumber]:getPos()
-		v.originUV = v.Part:getVertices(TEXTURE)[v.VertexNumber]:getUV()
-		v.originNormal = v.Part:getVertices(TEXTURE)[v.VertexNumber]:getNormal()
-	end
-end
-
-
-events.ENTITY_INIT:register(function()
-	addVerticeOriginVectors()
-end, "FoxTailHandler")
-
-
-local function applyVertexPairing(vertex_pairing)
-	
-end
 
 
 events.RENDER:register(function (delta, context, matrix)
+	do -- Example math code
+		local Theta = TAIL[1]:getRot():toRad()
 
-	for i = 0, #TAIL, 1 do
-		local Theta = TAIL[i]:getRot():toRad()
-		local r = vec(
-			(VERTICES[i.."UNW"].originPos.x - VERTICES[i.."UNE"].originPos.x) / 2, 
-			(VERTICES[i.."ENU"].originPos.y - VERTICES[i.."END"].originPos.y) / 2
-		)
+		local rX = TAIL[0]:getVertices(TEXTURE)[VERTICES["UNW"]]:getPos().x
+		local rx = TAIL[0]:getVertices(TEXTURE)[VERTICES["UNE"]]:getPos().x
+		local rY = TAIL[0]:getVertices(TEXTURE)[VERTICES["EUN"]]:getPos().y
+		local ry = TAIL[0]:getVertices(TEXTURE)[VERTICES["EDN"]]:getPos().y
+		local r = vec((rX - rx) / 2, (rY - ry) / 2)
 
-		VERTICES[i.."USW"].Part:getVertices(TEXTURE)[VERTICES[i.."USW"].VertexNumber]:setPos(
+		TAIL:getVertices(TEXTURE)[VERTICES["USW"]]:setPos(
 			math.cos(Theta.x) * r.x,
 			math.sin(Theta.y) * r.y,
 			math.sin(Theta.x) * r.x
 		)
 	end
 
-end, "FoxTailHandler")
 
 
 
 
---DEBUG
+end, "FoxTailHandler-RENDER")
+
+
+
+
+-- DEBUG
 local PART = TAIL[5].gapfill
 
 local FaceGroups = { {} }
@@ -185,11 +136,9 @@ events.KEY_PRESS:register(function(key, state, modifiers)
 		print(index)
 
 	elseif key == 305 and state == 1 then
-		
-		--printTable(TAIL[1].Base:getVertices(TEXTURE)[15], 2)
-		print(TAIL[1].Base:getVertices(TEXTURE)[15]:getPos())
-		print(TAIL[1].Base:getVertices(TEXTURE)[15]:getUV())
-		print(TAIL[1].Base:getVertices(TEXTURE)[15]:getNormal())
+
+		index = 0
+		face = 0
 
 	end
 end, "FoxTailHandler-DEBUG")
