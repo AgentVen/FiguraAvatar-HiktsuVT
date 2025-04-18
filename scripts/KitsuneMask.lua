823876fc3b76dc7cc6bbd4b7b0070b51929b36d5
AgentVen <agentven0x@gmail.com>
2025-04-18 15:18:25 -0600

    Started work on mask 'dangle' phyisics

--- scripts/KitsuneMask.lua
+++ scripts/KitsuneMask.lua
@@ -6,6 +6,7 @@ local MASK_FOX = models.models.mask_fox.root.Head
 
 local OFFSET_POSITION = vec(-1, 2, 0)
 local OFFSET_ROTATION = vec(21.444, 54.4363, 5.151)
+local DANGLE_OFFSET_ROTATION = vec(-17.5486, 0.7625, -3.6039)
 
 local MASK_STATE = 2	--todo)) Move this to some place where it can be more easly changed durring runtime.
 
@@ -40,8 +41,19 @@ MASK_FOX.strap_offtotheside:setVisible(false)
 MASK_FOX.mask_fox:setVisible(false)
 
 
+---@return Vector3
+local function dangleRightRotationPhysicsCorrection()
+	return vec(0, 0, 0)
+end
+
+---@return Vector3
+local function dangleLeftRotationPhysicsCorrection()
+
+	return vec(0, 0, 0)
+end
+
 -- State change handling on every RENDER event
-events.RENDER:register(function()
+events.RENDER:register(function(delta, renderContext, matrix)
 	-- Removed
 	if MASK_STATE == 0 then
 		MASK_FOX.strap:setVisible(false)
@@ -49,17 +61,22 @@ events.RENDER:register(function()
 		MASK_FOX.mask_fox:setVisible(false)
 	-- Covers face
 	elseif MASK_STATE == 1 then
-		MASK_FOX.mask_fox:setOffsetRot(0,0,0)
+		MASK_FOX.mask_fox:setRot(0,0,0)
 		MASK_FOX.mask_fox:setPos(0,0,0)
 		MASK_FOX.strap:setVisible(true)
 		MASK_FOX.strap_offtotheside:setVisible(false)
 		MASK_FOX.mask_fox:setVisible(true)
+		MASK_FOX.mask_fox.dangle_right:setRot(dangleRightRotationPhysicsCorrection())
+		MASK_FOX.mask_fox.dangle_left:setRot(dangleLeftRotationPhysicsCorrection())
 	-- Off to the side
 	elseif MASK_STATE == 2 then
-		MASK_FOX.mask_fox:setOffsetRot(OFFSET_ROTATION)
+		MASK_FOX.mask_fox:setRot(OFFSET_ROTATION)
 		MASK_FOX.mask_fox:setPos(OFFSET_POSITION)
 		MASK_FOX.strap:setVisible(false)
 		MASK_FOX.strap_offtotheside:setVisible(true)
 		MASK_FOX.mask_fox:setVisible(true)
+		MASK_FOX.mask_fox.dangle_right:setRot(DANGLE_OFFSET_ROTATION + dangleRightRotationPhysicsCorrection())
+		MASK_FOX.mask_fox.dangle_left:setRot(DANGLE_OFFSET_ROTATION + dangleLeftRotationPhysicsCorrection())
 	end
-end, "KitsuneMask")
+
+end, "KitsuneMask-RENDER")
