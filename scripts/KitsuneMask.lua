6702c94ca844001e990445aa9308d34546597ad8
AgentVen <agentven0x@gmail.com>
2025-04-18 18:03:12 -0600

    Mask 'dangles' phyisics now works, so far it can react to the player's head changing pitch

--- scripts/KitsuneMask.lua
+++ scripts/KitsuneMask.lua
@@ -4,9 +4,16 @@
 
 local MASK_FOX = models.models.mask_fox.root.Head
 
-local OFFSET_POSITION = vec(-1, 2, 0)
-local OFFSET_ROTATION = vec(21.444, 54.4363, 5.151)
+local MASK_FOX_OFFSET_POSITION = vec(-1, 2, 0)
+local MASK_FOX_OFFSET_ROTATION = vec(21.444, 54.4363, 5.151)
 local DANGLE_OFFSET_ROTATION = vec(-17.5486, 0.7625, -3.6039)
+local DANGLE_RIGHT_ORIGIN_POSITION = vec(4.15, 24.075, -4.6)
+local DANGLE_RIGHT_OFFSET_POSITION = vec(-6.4273, 3.575, -1.4928)
+local DANGLE_RIGHT_OFFSET_ROTATION = vec(0, 0, 0) -- y-35
+local DANGLE_LEFT_ORIGIN_POSITION = vec(-4.35, 24.075, -4.6)
+local DANGLE_LEFT_OFFSET_POSITION = vec(-2.8586, 3.125, 5.4241)
+local DANGLE_LEFT_OFFSET_ROTATION = vec(0, 0, 0) -- y-32.5
+
 
 local MASK_STATE = 2	--todo)) Move this to some place where it can be more easly changed durring runtime.
 
@@ -42,18 +49,83 @@ MASK_FOX.mask_fox:setVisible(false)
 
 
 ---@return Vector3
-local function dangleRightRotationPhysicsCorrection()
-	return vec(0, 0, 0)
+local function dangleRightRotationPhysicsCorrection(mask_state)
+	local headPitch = player:getRot().x
+
+	-- Isolating the x, so that we can use "If" statments
+	-- 
+	-- We clamp the x so that is looks like it gets cought on soild objects.
+	local x = 0
+	if mask_state == 1 then
+		x = math.clamp(headPitch, 0, 90)
+	elseif mask_state == 2 then
+		x = math.clamp(headPitch, -12, 90)
+	end
+
+	local newRot = vec(x, 0, 0)
+
+	-- Seperate adjust ment for the end part of the dangle, when the rest gets cought on a soild object, but it can keep going.
+	if (mask_state == 1) and headPitch < 0 then
+		MASK_FOX.mask_dangle_right.dangle_right:setRot(
+			headPitch,
+			0, 0
+		)
+	elseif mask_state == 2 then
+		if headPitch <= -12 then
+			MASK_FOX.mask_dangle_right.dangle_right:setRot(
+				math.clamp(headPitch, -10, 90),
+				0, 0
+			)
+		else
+			MASK_FOX.mask_dangle_right.dangle_right:setRot(0, 0, 0)
+		end
+	end
+
+	return newRot
 end
 
 ---@return Vector3
-local function dangleLeftRotationPhysicsCorrection()
+local function dangleLeftRotationPhysicsCorrection(mask_state)
+	local headPitch = player:getRot().x
 
-	return vec(0, 0, 0)
+	-- Isolating the x, so that we can use "If" statments
+	-- 
+	-- We clamp the x so that is looks like it gets cought on soild objects.
+	local x = 0
+	if mask_state == 1 then
+		x = math.clamp(headPitch, 0, 90)
+	elseif mask_state == 2 then
+		x = math.clamp(headPitch, -90, 11)
+	end
+
+	local newRot = vec(x, 0, 0)
+
+	-- Seperate adjust ment for the end part of the dangle, when the rest gets cought on a soild object, but it can keep going.
+	if (mask_state == 1) and headPitch < 0 then
+		MASK_FOX.mask_dangle_left.dangle_left:setRot(
+			headPitch,
+			0, 0
+		)
+	elseif mask_state == 2 then
+		if headPitch >= 11 then
+			MASK_FOX.mask_dangle_left.dangle_left:setRot(
+				headPitch - newRot.x,
+				0, 0
+			)
+		else
+			MASK_FOX.mask_dangle_right.dangle_right:setRot(0, 0, 0)
+		end
+	end
+
+	return newRot
 end
 
 -- State change handling on every RENDER event
 events.RENDER:register(function(delta, renderContext, matrix)
+
+	--print(MASK_FOX.mask_dangle_right:getPos())
+	--print(MASK_FOX.mask_dangle_left:getPos())
+
 	-- Removed
 	if MASK_STATE == 0 then
 		MASK_FOX.strap:setVisible(false)
@@ -61,22 +133,26 @@ events.RENDER:register(function(delta, renderContext, matrix)
 		MASK_FOX.mask_fox:setVisible(false)
 	-- Covers face
 	elseif MASK_STATE == 1 then
-		MASK_FOX.mask_fox:setRot(0,0,0)
-		MASK_FOX.mask_fox:setPos(0,0,0)
+		MASK_FOX.mask_fox:setPos(0, 0, 0)
+		MASK_FOX.mask_fox:setRot(0, 0, 0)
+		MASK_FOX.mask_dangle_right:setPos(0, 0, 0)
+		MASK_FOX.mask_dangle_right:setRot(dangleRightRotationPhysicsCorrection(MASK_STATE))
+		MASK_FOX.mask_dangle_right:setPos(0, 0, 0)
+		MASK_FOX.mask_dangle_left:setRot(dangleLeftRotationPhysicsCorrection(MASK_STATE))
 		MASK_FOX.strap:setVisible(true)
 		MASK_FOX.strap_offtotheside:setVisible(false)
 		MASK_FOX.mask_fox:setVisible(true)
-		MASK_FOX.mask_fox.dangle_right:setRot(dangleRightRotationPhysicsCorrection())
-		MASK_FOX.mask_fox.dangle_left:setRot(dangleLeftRotationPhysicsCorrection())
 	-- Off to the side
 	elseif MASK_STATE == 2 then
-		MASK_FOX.mask_fox:setRot(OFFSET_ROTATION)
-		MASK_FOX.mask_fox:setPos(OFFSET_POSITION)
+		MASK_FOX.mask_fox:setPos(MASK_FOX_OFFSET_POSITION)
+		MASK_FOX.mask_fox:setRot(MASK_FOX_OFFSET_ROTATION)
+		MASK_FOX.mask_dangle_right:setPos(DANGLE_RIGHT_OFFSET_POSITION)
+		MASK_FOX.mask_dangle_right:setRot(DANGLE_RIGHT_OFFSET_ROTATION + dangleRightRotationPhysicsCorrection(MASK_STATE))
+		MASK_FOX.mask_dangle_left:setPos(DANGLE_LEFT_OFFSET_POSITION)
+		MASK_FOX.mask_dangle_left:setRot(DANGLE_LEFT_OFFSET_ROTATION + dangleLeftRotationPhysicsCorrection(MASK_STATE))
 		MASK_FOX.strap:setVisible(false)
 		MASK_FOX.strap_offtotheside:setVisible(true)
 		MASK_FOX.mask_fox:setVisible(true)
-		MASK_FOX.mask_fox.dangle_right:setRot(DANGLE_OFFSET_ROTATION + dangleRightRotationPhysicsCorrection())
-		MASK_FOX.mask_fox.dangle_left:setRot(DANGLE_OFFSET_ROTATION + dangleLeftRotationPhysicsCorrection())
 	end
 
 end, "KitsuneMask-RENDER")
